6f3f85584f79d0ff1a6cd137b34482c610125a2f
bit-user <bit-user@bit>
2016-11-03 15:19:39 +0900

    spring guestbook

---
+++ src/main/java/com/bit2016/guestbook/controller/GuestBookController.java
@@ -0,0 +1,46 @@
+package com.bit2016.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bit2016.guestbook.DAO.GuestBookDAO;
+import com.bit2016.guestbook.VO.GuestBook;
+
+@Controller
+public class GuestBookController {
+	@Autowired
+	private GuestBookDAO guestBookDAO;
+
+	@RequestMapping("")
+	public String list(Model model) {
+		List<GuestBook> list = guestBookDAO.selectAll();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/list.jsp";
+	}
+
+	@RequestMapping("/add")
+	public String add(@ModelAttribute GuestBook vo) {
+		guestBookDAO.insert(vo);
+
+		return "redirect:/";
+	}
+
+	@RequestMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		guestBookDAO.delete(id);
+		return "redirect:/";
+	}
+
+	@RequestMapping("/deleteform/{id}")
+	public String deleteForm(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("id", id);
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+}
